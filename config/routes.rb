b2e17767a5b6c308fd2bd099a922733d00368661
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-23 15:44:44 +0200

    missing desing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/task"
+  get "/tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "task/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
